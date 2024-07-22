b3dbc08f24d6bd2dde2cacf9faaa667fff1fb5aa
Kornelijus.semenas <kornelijus.semenas@omega365.com>
2024-07-22 11:58:24 +0200

    test - fix

--- src/main.ts
+++ src/main.ts
@@ -79,7 +79,7 @@ omega365IDE.initialize(document.getElementById("app")!).then(async () => {
 
     omega365IDE.fileSystemProvider.registerFile(new Omega365IDE.RegisteredMemoryFile(workspaceUri, JSON.stringify({
         folders: [{
-            path: `/Omega365/project`
+            path: `/Omega365/`
         }]
     }, null, 2)))
 });
